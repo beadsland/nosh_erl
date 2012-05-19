204ae098ee9d1fb8ab272d7638dfed942caa74b7
Beads D. Land-Trujillo <beads.d.land@gmail.com>
2012-05-19 15:49:32 -0400

    start entry point

--- doc/bang.md
+++ doc/bang.md
@@ -3,6 +3,7 @@
 #Module ?module#
 
 * [Description](#description)
+* [Data Types](#types)
 * [Function Index](#index)
 * [Function Details](#functions)
 
@@ -13,12 +14,33 @@ Copyright (c) 2012 Beads D. Land-Trujillo
 
 __Version:__ 0.2.0
 
-__Authors:__ Beads D. Land-Trujillo (_web site:_ [`http://twitter.com/beadsland`](http://twitter.com/beadsland)).<a name="index"></a>
+__Authors:__ Beads D. Land-Trujillo (_web site:_ [`http://twitter.com/beadsland`](http://twitter.com/beadsland)).
+<a name="types"></a>
+
+##Data Types##
+
+
+
+
+###<a name="type-bang_error">bang_error()</a>##
+
+
+
+<pre>bang_error() = failed_open_port | {exit, any()}</pre>
+
+
+
+###<a name="type-bang_status">bang_status()</a>##
+
+
+
+<pre>bang_status() = ok | {status, integer()}</pre>
+<a name="index"></a>
 
 ##Function Index##
 
 
-<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#run-3">run/3</a></td><td>Run an OS command as a pose command.</td></tr></table>
+<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#run-3">run/3</a></td><td>Run an OS command as a pose command.</td></tr><tr><td valign="top"><a href="#start-1">start/1</a></td><td>Start bang as a blocking function.</td></tr></table>
 
 
 <a name="functions"></a>
@@ -34,4 +56,13 @@ __Authors:__ Beads D. Land-Trujillo (_web site:_ [`http://twitter.com/beadsland`
 <br></br>
 
 
-Run an OS command as a pose command.
+Run an OS command as a pose command.<a name="start-1"></a>
+
+###start/1##
+
+
+<pre>start(Line::[atom()]) -> <a href="#type-bang_status">bang_status()</a> | <a href="#type-bang_error">bang_error()</a></pre>
+<br></br>
+
+
+Start bang as a blocking function.
--- src/bang.erl
+++ src/bang.erl
@@ -51,20 +51,30 @@
 
 -import(strings).
 -import(erlang).
+-import(io).
 
 %%
 %% Exported Functions
 %%
 
 % API export
--export([run/3]).
+-export([start/1, run/3]).
 
 % hidden
--export([bang_loop/3]).
+-export([bang_loop/3, loop/2]).
 
 %%
 %% API Functions
 %%
+
+-type bang_error() :: failed_open_port | {exit, any()}.
+-type bang_status() :: ok | {status, integer()}.
+-spec start([atom()]) -> bang_status() | bang_error().
+%% @doc Start bang as a blocking function.
+start(Line) ->
+  IO = ?IO(self()),
+  RunPid = spawn_link(?MODULE, run, [IO, ?ARG(?MODULE, Line), ?ENV]),
+  ?MODULE:loop(IO, RunPid).
 
 -spec run(IO :: #std{}, ARG :: #arg{}, ENV :: #env{}) -> no_return().
 %% @doc Run an OS command as a pose command.
@@ -99,10 +109,40 @@ bang_loop(IO, Port, Timeout) ->
     {Port, {exit_status, 0}}        -> exit(ok);
     {Port, {exit_status, Status}}   -> exit({status, Status});
     {'EXIT', Port, normal}          -> exit(ok);
-    {'EXIT', Port, Reason}          -> exit(Reason);
+    {'EXIT', Port, Reason}          -> exit({exit, Reason});
     Noise                           -> ?STDERR("noise: ~p ~p~n",
                                                [Noise, self()]),
                                        ?MODULE:bang_loop(IO, Port, Timeout)
   after Timeout ->
       exit(timeout)
   end.
+
+%%%
+% Start loop
+%%%
+
+% @hidden Export to allow for hotswap.
+loop(IO, RunPid) ->
+  receive
+    {purging, _Pid, _Mod}       -> ?MODULE:loop(IO, RunPid);
+    {'EXIT', RunPid, Reason}    -> Reason;
+    {MsgTag, RunPid, Line}      -> do_output(MsgTag, Line),
+                                   ?MODULE:loop(IO, RunPid);
+    Noise                       -> do_noise(Noise),
+                                   ?MODULE:loop(IO, RunPid)
+  end.
+
+% Handle stderr and stdout messages.
+do_output(MsgTag, Output) ->
+  case MsgTag of
+    stdout  -> io:format("~s", [Output]);
+    erlout  -> io:format("~p: data: ~p~n", [?MODULE, Output]);
+    erlerr  -> Erlerr = ?FORMAT_ERLERR(Output),
+               io:format(standard_error, "** ~s~n", [Erlerr]);
+    stderr  -> io:format(standard_error, "** ~s", [Output]);
+    debug   -> io:format(standard_error, "-- ~s", [Output])
+  end.
+
+% Handle message queue noise.
+do_noise(Noise) ->
+  io:format(standard_error, "noise: ~p ~p~n", [Noise, self()]).
