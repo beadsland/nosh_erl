d3e340eac40b9d8ee5d107aa4096e2411ef6ff46
beadsland <beads.d.land@gmail.com>
2013-04-11 10:51:00 -0400

    command to launch crashdump_viewer

---
+++ src/cdv.erl
@@ -0,0 +1,106 @@
+%% CDDL HEADER START    -*-Erlang-*-
+%% -----------------------------------------------------------------------
+%% The contents of this file are subject to the Common Development and
+%% Distribution License, Version 1.0 (the "License"); you may not use
+%% this file except in compliance with the License.  You should have
+%% received a copy of the Common Development and Distribution License
+%% along with this software.  If not, it can be retrieved online at
+%% http://www.opensource.org/licenses/CDDL-1.0
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% When distributing Covered Code, include this CDDL Header Notice in
+%% each file and include the License file at CDDL-LICENSE.  If applicable
+%% add the following below the CDDL Header, with the fields enclosed
+%% by brackets replaced by your own identifying information:
+%% "Portions Copyright [year] [name of copyright owner]"
+%%
+%% Copyright 2012, 2013 Beads D. Land-Trujillo.  All Rights Reserved.
+%% -----------------------------------------------------------------------
+%% CDDL HEADER END
+
+%% @reference This command makes use of the `pose_code' module of the
+%% <a href="http://github.com/beadsland/pose">POSIX-like interface
+%% emulation</a>.
+%% @end
+%% @reference The <a href="http://www.erlang.org/doc/man/crashdump_viewer.html">
+%% crashdump_viewer</a> application is a Web-based tool for reviewing Erlang
+%% <code>erl_crash.dump</code> files.
+%% @end
+%% @doc Launch <code>crashdump_viewer</code> application.
+%% @end
+%% @author Beads D. Land-Trujillo [http://twitter.com/beadsland]
+%% @copyright 2012 Beads D. Land-Trujillo
+
+%% @version 0.0.1
+
+-define(module, cdv).
+
+% BEGIN POSE PACKAGE PATTERN
+-ifndef(package).
+-module(?module).
+-package(default).
+-else.
+-module(?package.?module).
+-package(?package).
+-endif.
+% END POSE PACKAGE PATTERN
+
+-version("0.0.1").
+
+%%
+%% Include files
+%%
+
+%-define(debug, true).
+-include_lib("pose/include/interface.hrl").
+
+-import(gen_command).
+-import(pose).
+-import(pose_command).
+
+%%
+%% Exported Functions
+%%
+
+-behaviour(gen_command).
+
+% API entry points
+-export([start/0, start/1, run/3]).
+
+% private callbacks
+-export([do_run/2]).
+
+%%
+%% API Functions
+%%
+
+-spec start() -> no_return().
+%% @equiv start([])
+start() -> start([]).
+
+-spec start(Param :: [atom()]) -> no_return().
+%% @doc Start as a blocking function.
+start(Param) -> gen_command:start(Param, ?MODULE).
+
+-spec run(IO :: #std{}, ARG :: #arg{}, ENV :: #env{}) -> no_return().
+%% doc Start as a `pose' command.
+run(IO, ARG, ENV) -> gen_command:run(IO, ARG, ENV, ?MODULE).
+
+%%
+%% Callback Functions
+%%
+
+%% @private Callback entry point for gen_command behaviour.
+%% @todo capture crashdump console output
+do_run(IO, _ARG) ->
+  ?STDOUT("Launching crashdump_viewer\n"),
+  Status = crashdump_viewer:start(),
+  exit(Status).
+
+%%
+%% Local Functions
+%%
